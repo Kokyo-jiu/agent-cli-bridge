0b81437aac37ea754400a87310c8fec66c703c66
Kokyo-jiu <cenjiu48@gmail.com>
2026-09-04 13:46:54 +0800

    Preserve package exports

--- src/agent_cli_bridge/__init__.py
+++ src/agent_cli_bridge/__init__.py
@@ -1,4 +1,5 @@
 from .core import *  # noqa: F401,F403
+from .core import __all__ as _core_all
 from .subprocess_transport import SubprocessPromptTransport
 
-__all__ = [*globals().get("__all__", []), "SubprocessPromptTransport"]
+__all__ = [*_core_all, "SubprocessPromptTransport"]
